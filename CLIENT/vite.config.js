5913fae09fb36c0ef80d3de289742efd5cfdff63
Margan Das <152724498+Mr-Nobody003@users.noreply.github.com>
2025-12-30 22:10:45 +0530

    Set Vite base path dynamically for GitHub Pages
    The base path in vite.config.js is now determined by the GITHUB_PAGES environment variable, allowing for easier local development and deployment to GitHub Pages.

--- CLIENT/vite.config.js
+++ CLIENT/vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+const base = isGithubPages ? "/MARGAN_DAS/" : "/";
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/MARGAN_DAS/',
+  base,
   plugins: [tailwindcss(),react()],
 })
